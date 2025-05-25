113812c5d5d444f5efbd617f43f3b358b7bcc3e5
Deccy92 <deccy_gregg@hotmail.com>
2025-05-25 15:43:01 +1000

    Update camera.py

--- camera.py
+++ camera.py
@@ -1,18 +1,26 @@
 import subprocess
 
-class VideoCamera:
-    def __init__(self):
+"""
+subprocess is a python module that allows the programmer to run linux shell commands inside the python script.
+This allows low-level hardware interaction with fast and effective linux commands.
+
+The main subprocess funciton used in this program is:
+    'subprocess.Popen()' - executes a command and lets the program interact with it while it is running - this enables continuous access to its output.
+"""
+
+class VideoCamera: #This is defining a class called VideoCamera
+    def __init__(self): #This constructor function is called whenever a program creates a VideoCamera object, executing the below code
         self.process = subprocess.Popen(
             [
-                "rpicam-vid",
-                "-t", "0",  # Run indefinitely
-                "--post-process-file", "/usr/share/rpi-camera-assets/imx500_mobilenet_ssd.json",
-                "--codec", "mjpeg",
-                "-o", "-"
+                "rpicam-vid", #Starts the video capture
+                "-t", "0",  #Tells the camera to run indefinitely
+                "--post-process-file", "/usr/share/rpi-camera-assets/imx500_mobilenet_ssd.json", #This command is taken straight from the Raspberry Pi AI Camera documentation - it runs rpicam-vid with object detection post-processing
+                "--codec", "mjpeg", #This tells the camera to encode the data as mjpeg
+                "-o", "-" #Sends the stream directly to stdout (standard output), not a file
             ],
-            stdout=subprocess.PIPE
+            stdout=subprocess.PIPE #Captures the output so Python can read it as a live stream
         )
-        self.buffer = b""
+        self.buffer = b"" #A byte buffer to store incoming stream data and extract full JPEGs
 
     def get_frame(self):
         # Collect data until a full JPEG frame is found
@@ -32,3 +40,5 @@ class VideoCamera:
     def __del__(self):
         if self.process:
             self.process.terminate()
+
+
